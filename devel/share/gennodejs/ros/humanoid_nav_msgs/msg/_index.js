f1a1b97c67c4b8240966a2edd168ad95bda370cf
Lexington2002 <3312853823@qq.com>
2023-04-14 08:14:38 +0800

    🎉 init: 初始化

---
+++ devel/share/gennodejs/ros/humanoid_nav_msgs/msg/_index.js
@@ -0,0 +1,22 @@
+
+"use strict";
+
+let StepTarget = require('./StepTarget.js');
+let ExecFootstepsAction = require('./ExecFootstepsAction.js');
+let ExecFootstepsResult = require('./ExecFootstepsResult.js');
+let ExecFootstepsFeedback = require('./ExecFootstepsFeedback.js');
+let ExecFootstepsActionGoal = require('./ExecFootstepsActionGoal.js');
+let ExecFootstepsActionFeedback = require('./ExecFootstepsActionFeedback.js');
+let ExecFootstepsGoal = require('./ExecFootstepsGoal.js');
+let ExecFootstepsActionResult = require('./ExecFootstepsActionResult.js');
+
+module.exports = {
+  StepTarget: StepTarget,
+  ExecFootstepsAction: ExecFootstepsAction,
+  ExecFootstepsResult: ExecFootstepsResult,
+  ExecFootstepsFeedback: ExecFootstepsFeedback,
+  ExecFootstepsActionGoal: ExecFootstepsActionGoal,
+  ExecFootstepsActionFeedback: ExecFootstepsActionFeedback,
+  ExecFootstepsGoal: ExecFootstepsGoal,
+  ExecFootstepsActionResult: ExecFootstepsActionResult,
+};
