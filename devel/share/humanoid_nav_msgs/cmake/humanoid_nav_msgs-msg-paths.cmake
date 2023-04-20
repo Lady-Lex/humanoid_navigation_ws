cc849c6f1913f996c853d519306a451df2dd8453
Lexington2002 <3312853823@qq.com>
2023-04-20 13:12:59 +0800

    🎉 init: github提交初始化

--- devel/share/humanoid_nav_msgs/cmake/humanoid_nav_msgs-msg-paths.cmake
+++
@@ -1,4 +0,0 @@
-# generated from genmsg/cmake/pkg-msg-paths.cmake.develspace.in
-
-set(humanoid_nav_msgs_MSG_INCLUDE_DIRS "/home/lexington2002/Workspace/humanoid_navigation_ws/src/humanoid_msgs/humanoid_nav_msgs/msg;/home/lexington2002/Workspace/humanoid_navigation_ws/devel/share/humanoid_nav_msgs/msg")
-set(humanoid_nav_msgs_MSG_DEPENDENCIES std_msgs;geometry_msgs;actionlib_msgs)
